8bd9fecb47090f32f6d32e49bf1b72bd137d171a
Admin <btaylor@allytechllc.com>
2023-09-29 15:46:07 -0400

    This is the REAL final code for section 2, step 14

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cubit2cubit/cubits/counter/counter_cubit.dart';
+import 'package:cubit2cubit/cubits/color/color_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,16 +12,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'cubit2cubit',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ColorCubit>(create: (context) => ColorCubit()),
+        BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(
+            colorCubit: context.read<ColorCubit>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'cubit2cubit',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
-  }
-}
+  } //Widget
+} //class MyApp
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -26,7 +39,10 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: context
+          .watch<ColorCubit>()
+          .state
+          .color, // backgroundColor is using the Bloc extenstion method to listen for color change
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -36,11 +52,16 @@ class MyHomePage extends StatelessWidget {
                 'Change Color',
                 style: TextStyle(fontSize: 24.0),
               ),
-              onPressed: () {},
+              onPressed: () {
+                context
+                    .read<ColorCubit>()
+                    .changeColor(); // uses Bloc extension method to
+                // context.read (provide) to the cubit the request for color state change
+              },
             ),
             SizedBox(height: 20.0),
             Text(
-              '0',
+              '${context.watch<CounterCubit>().state.counter}', // uses Bloc extension method context.watch to update the text for the counter.
               style: TextStyle(
                 fontSize: 52.0,
                 fontWeight: FontWeight.bold,
@@ -53,7 +74,9 @@ class MyHomePage extends StatelessWidget {
                 'Increment Counter',
                 style: TextStyle(fontSize: 24.0),
               ),
-              onPressed: () {},
+              onPressed: () {
+                context.read<CounterCubit>().changeCounter();
+              },
             ),
           ],
         ),
@@ -61,3 +84,27 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+/*
+// *** This is the extension method with nested BlocProviders ***
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ColorCubit(),
+      child: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(
+          // two ways to handle this
+          // this is the extension method
+          colorCubit: context.read<ColorCubit>(),
+        ),
+        child: MaterialApp(
+          title: 'cubit2cubit',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(),
+        ),
+      ),
+    );
+  } //Widget
+} //class MyApp
+*/
